9d5154683191a5f49df749947b61a441d49ffb67
Krashang Kumar <krashang.kumar@mail.vinove.com>
2025-11-17 12:49:50 +0530

    truncate

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -3,6 +3,11 @@
 import { Button } from "@heroui/react";
 
 export default function PostCard({ post }: any) {
+  const truncatedContent =
+    post.content.length > 150
+      ? post.content.slice(0, 150) + "..."
+      : post.content;
+
   return (
     <div
       className="
@@ -15,13 +20,15 @@ export default function PostCard({ post }: any) {
         {post.title}
       </h2>
 
-      <p className="text-gray-600 mt-2 line-clamp-3 text-sm">{post.content}</p>
+      <p className="text-gray-600 mt-2 line-clamp-3 text-sm">
+        {truncatedContent}
+      </p>
 
       <div className="mt-4">
         <Button
           className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-medium px-4 py-2"
           radius="sm"
-          onPress={() => window.location.href = `/posts/${post._id}`}
+          onPress={() => (window.location.href = `/posts/${post._id}`)}
         >
           Read More
         </Button>
